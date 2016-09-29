b0660c3a01a620e3ef861b3ebeb4caf93f9be3c0
Carlos Jurado Moreno <Carlos Jurado Moreno>
2016-09-29 11:10:07 +0200

    Ejercicios

---
+++ Tema 4/ej6t4.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+	<title>Contador</title>
+</head>
+<body>
+<h4>Ejercicio 6</h4>
+	Muestra los números del 320 al 160, contando de 20 en 20 utilizando un bucle do-while.<br>
+
+
+
+	<?php
+	$i=320;
+	do{
+		echo $i . "<br>";
+		$i-=20;
+	}while ($i>=160);
+
+
+	?>
+</body>
+</html>
---
+++ Tema 4/ej7t4.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+	<title>Contador</title>
+</head>
+<body>
+<h4>Ejercicio 7</h4>
+	Realiza el control de acceso a una caja fuerte. La combinación será un número de 4 cifras. El
+	programa nos pedirá la combinación para abrirla. Si no acertamos, se nos mostrará el mensaje
+	“Lo siento, esa no es la combinación” y si acertamos se nos dirá “La caja fuerte se ha abierto
+	satisfactoriamente”. Tendremos cuatro oportunidades para abrir la caja fuerte.<br>
+
+
+
+	<?php
+	?>
+</body>
+</html>
